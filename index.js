049bab37bf782e0c66784595605f294094bdb3da
Alex Kotliarskyi <alex.frantic@gmail.com>
2015-07-24 01:33:33 -0700

    Highlight changed lines

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 var fs = require('fs');
 var path = require('path');
-var server = require('http').createServer(handleRequest);
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
 var io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 function handleRequest(req, res) {
   fs.readFile(path.join(__dirname, 'index.html'), function(err, page) {
     if (err) {
-      throw err;
+      throw err; //1
     }
 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(page);
   });
+  // x
 }
 
-var fileName = __filename;
+var fileName = 'public/index.html';
 
 function getSnapshot() {
   return {
@@ -29,7 +36,7 @@ io.on('connection', function (socket) {
   socket.emit('init', snapshots);
 });
 
-fs.watch(__filename, function() {
+fs.watch(fileName, function() {
   var last = snapshots[snapshots.length - 1];
   var current = getSnapshot();
   if (current.content !== last.content) {
