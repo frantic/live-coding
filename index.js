644460652d5b5dda310936ad4d225b70912d3834
Alex Kotliarskyi <alex.frantic@gmail.com>
2015-07-23 23:40:09 -0700

    Simple socket.io integration

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+var fs = require('fs');
+var path = require('path');
 var server = require('http').createServer(handleRequest);
 var io = require('socket.io')(server);
 
 function handleRequest(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('OK');
+  fs.readFile(path.join(__dirname, 'index.html'), function(err, page) {
+    if (err) {
+      throw err;
+    }
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(page);
+  });
 }
 
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+console.log('Listening http://localhost:3000/');
 server.listen(3000);
